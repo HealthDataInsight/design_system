bb182eab5c99ee7242cc0060b4f5de661ba5ee8f
Tim Gentry <52189+timgentry@users.noreply.github.com>
2024-05-07 10:44:21 +0100

    Moved two images and added the Rack::Static initialiser

--- lib/design_system/engine.rb
+++ lib/design_system/engine.rb
@@ -1,6 +1,18 @@
+# require 'sprockets/railtie'
+# require 'sprockets/rails/quiet_assets'
+
 module DesignSystem
   class Engine < ::Rails::Engine
     # Allow changes to the design system to be reloaded in development.
     config.autoload_paths << File.expand_path('..', __dir__) if Rails.env.development?
+
+    initializer 'design_system.add_middleware' do |app|
+      app.middleware.insert_before(
+        ::Sprockets::Rails::QuietAssets,
+        ::Rack::Static,
+        urls: ['/design_system'],
+        root: DesignSystem::Engine.root.join('public')
+      )
+    end
   end
 end
