72c2d2341fb1435dc76be9043cd48c2e89a7efca
Tim Gentry <52189+timgentry@users.noreply.github.com>
2024-05-20 17:18:27 +0100

    Added ActiveRecord and Assistants resource

--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -1,5 +1,21 @@
+# This is the application controller
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session if Rails.env.test?
+
   include DesignSystem::Branded
+
+  before_action :add_navigation
+
+  private
+
+  def add_navigation
+    add_navigation_item('Manage Assistants', assistants_path)
+
+    add_navigation_item('GOV.UK', url_for(brand: 'govuk'))
+    add_navigation_item('HDI', url_for(brand: 'hdi'))
+    add_navigation_item('NDRS', url_for(brand: 'ndrsuk'))
+    add_navigation_item('NHS', url_for(brand: 'nhsuk'))
+  end
 
   def brand
     @brand ||= 'nhsuk'
--- test/dummy/app/controllers/pages_controller.rb
+++ test/dummy/app/controllers/pages_controller.rb
@@ -1,12 +1,4 @@
+# This is the pages controller
 class PagesController < ApplicationController
-  before_action :add_navigation
-
   def index; end
-
-  def add_navigation
-    add_navigation_item('GOV.UK', url_for(brand: 'govuk'))
-    add_navigation_item('HDI', url_for(brand: 'hdi'))
-    add_navigation_item('NDRS', url_for(brand: 'ndrsuk'))
-    add_navigation_item('NHS', url_for(brand: 'nhsuk'))
-  end
 end
