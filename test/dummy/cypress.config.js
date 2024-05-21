8910aa6e620d41e18ef21112e4fabd1838703483
Tim Gentry <52189+timgentry@users.noreply.github.com>
2024-05-21 10:39:52 +0100

    Javascript linting

--- test/dummy/cypress.config.js
+++ test/dummy/cypress.config.js
@@ -1,13 +1,13 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000', //DevSKIM: ignore DS137138
-    setupNodeEvents(on, config) {
+    baseUrl: 'http://localhost:3000', // DevSKIM: ignore DS137138
+    setupNodeEvents (on, config) {
       // implement node event listeners here
-    },
+    }
   },
-  screenshotsFolder: "tmp/cypress_screenshots",
-  videosFolder: "tmp/cypress_videos",
-  trashAssetsBeforeRuns: false 
-});
+  screenshotsFolder: 'tmp/cypress_screenshots',
+  videosFolder: 'tmp/cypress_videos',
+  trashAssetsBeforeRuns: false
+})
