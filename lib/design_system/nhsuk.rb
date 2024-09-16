8e8e88585c57fc48e008b22cd691c9d70b2cc584
Tim Gentry <52189+timgentry@users.noreply.github.com>
2024-09-16 15:38:23 +0100

    NHS

--- lib/design_system/nhsuk.rb
+++ lib/design_system/nhsuk.rb
@@ -1,18 +1,12 @@
-require_relative 'govuk'
-require_relative 'builders/nhsuk/breadcrumbs'
+# frozen_string_literal: true
+
+require_relative 'builders/nhsuk/fixed_elements'
 require_relative 'builders/nhsuk/table'
 
 # Extend the design system module to include Nhsuk
 module DesignSystem
   # This is the NHSUK adapter for the design system
-  class Nhsuk < Govuk
-    include Builders::Nhsuk::Breadcrumbs
-
-    # private
-
-    # def render_main_container(&)
-    #   content_tag(:div, class: "#{brand}-width-container", &)
-    # end
+  class Nhsuk
   end
 
   Registry.register(Nhsuk)
