092568f6e9e33d677957caad6cf99e7b2c2c2df5
Tim Gentry <52189+timgentry@users.noreply.github.com>
2024-09-16 20:12:09 +0100

    require form_builders

--- lib/design_system/hdi.rb
+++ lib/design_system/hdi.rb
@@ -4,5 +4,6 @@
 
 require_relative 'builders/hdi/fixed_elements'
 require_relative 'builders/hdi/table'
+require_relative 'form_builders/hdi'
 
 DesignSystem::Registry.register('hdi')
--- lib/design_system/ndrsuk.rb
+++ lib/design_system/ndrsuk.rb
@@ -1,8 +1,10 @@
-require_relative 'nhsuk'
+# frozen_string_literal: true
 
 # This is the NDRS branding adapter for the design system
 
+require_relative 'nhsuk'
 require_relative 'builders/ndrsuk/fixed_elements'
 require_relative 'builders/ndrsuk/table'
+require_relative 'form_builders/ndrsuk'
 
 DesignSystem::Registry.register('ndrsuk')
