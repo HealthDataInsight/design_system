0b9d79b08646bfc13678148ee3bfb20960d88306
Tim Gentry <52189+timgentry@users.noreply.github.com>
2024-09-17 08:57:06 +0100

    Interim

---
+++ lib/design_system/form_builders/hdi.rb
@@ -0,0 +1,10 @@
+require_relative 'base'
+
+module DesignSystem
+  # Module namespace containing all the form builders
+  module FormBuilders
+    # The HDI version of the form builder
+    class Hdi < Base
+    end
+  end
+end
