2c004f166cf8132c105f4278659e17243ed4796b
shilpigoel <shilpi.goel1@nhs.net>
2024-09-25 11:22:03 +0100

    Updated generic table to be plain non-branded one

--- lib/design_system/builders/govuk/table.rb
+++ lib/design_system/builders/govuk/table.rb
@@ -9,6 +9,22 @@ module DesignSystem
       class Table < ::DesignSystem::Builders::Generic::Table
         private
 
+        def table_content
+          content_tag(:table, nil, class: "#{brand}-table") do
+            safe_buffer = ActiveSupport::SafeBuffer.new
+
+            if @table.caption
+              safe_buffer.concat(content_tag(:caption, @table.caption,
+                                             class: "#{brand}-table__caption"))
+            end
+            safe_buffer.concat(render_headers)
+            safe_buffer.concat(render_rows)
+
+            safe_buffer
+          end
+        end
+
+        # brand specific <th>
         def render_headers
           content_tag(:thead, class: "#{brand}-table__head") do
             content_tag(:tr, class: "#{brand}-table__row") do
@@ -19,12 +35,47 @@ module DesignSystem
           end
         end
 
+        def render_header_cell(cell, scope)
+          classes = "#{brand}-table__header"
+          classes += " #{brand}-table__header--numeric" if cell_numeric?(cell)
+
+          content_tag(:th, cell[:content], cell[:options].merge(scope:, class: classes))
+        end
+
+        # brand specific <tr>
+        def render_rows
+          content_tag(:tbody, class: "#{brand}-table__body") do
+            @table.rows.each_with_object(ActiveSupport::SafeBuffer.new) do |row, rows_buffer|
+              rows_buffer.concat(render_row(row))
+            end
+          end
+        end
+
         def render_row(row)
           content_tag(:tr, class: "#{brand}-table__row") do
             row.each_with_object(ActiveSupport::SafeBuffer.new).with_index do |(cell, cell_buffer), index|
               cell_buffer.concat(render_cell(cell, index, 'row'))
             end
           end
+        end
+
+        def render_cell(cell, index, scope)
+          if index.zero?
+            render_header_cell(cell, scope)
+          else
+            render_data_cell(cell)
+          end
+        end
+
+        def render_data_cell(cell)
+          classes = "#{brand}-table__cell"
+          classes += " #{brand}-table__cell--numeric" if cell_numeric?(cell)
+
+          content_tag(:td, cell[:content], cell[:options].merge(class: classes))
+        end
+
+        def cell_numeric?(cell)
+          cell[:options][:type] == 'numeric'
         end
       end
     end
