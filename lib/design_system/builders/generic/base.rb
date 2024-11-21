84fe43fd09a5e4a448df95a0ee96f81d6590fd5b
shilpigoel <shilpi.goel1@nhs.net>
2024-11-21 15:04:10 +0000

    PR review comments incorporated

--- lib/design_system/builders/generic/base.rb
+++ lib/design_system/builders/generic/base.rb
@@ -6,7 +6,7 @@ module DesignSystem
       # This is the base class for design system builders.
       class Base
         include CssHelper
-        delegate :capture, :content_for, :content_tag, :link_to, :link_to_unless_current, :button_tag, to: :@context
+        delegate :button_tag, :capture, :content_for, :content_tag, :link_to, :link_to_unless_current, to: :@context
 
         def initialize(context)
           @context = context
