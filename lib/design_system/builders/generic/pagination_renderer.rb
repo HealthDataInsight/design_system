3000be5c8c06e8c4d1d50badbd02719f303985b5
shilpigoel <shilpi.goel1@nhs.net>
2024-12-02 11:10:19 +0000

    Removed html_safe

--- lib/design_system/builders/generic/pagination_renderer.rb
+++ lib/design_system/builders/generic/pagination_renderer.rb
@@ -61,7 +61,7 @@ module DesignSystem
           <svg class="#{brand}-icon nhsuk-icon__arrow-left" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" width="34" height="34">
           <path d="M4.1 12.3l2.7 3c.2.2.5.2.7 0 .1-.1.1-.2.1-.3v-2h11c.6 0 1-.4 1-1s-.4-1-1-1h-11V9c0-.2-.1-.4-.3-.5h-.2c-.1 0-.3.1-.4.2l-2.7 3c0 .2 0 .4.1.6z"></path>
           </svg>
-          ).html_safe
+          )
         end
 
         # SVG for the "Next" icon
@@ -70,7 +70,7 @@ module DesignSystem
           <svg class="#{brand}-icon nhsuk-icon__arrow-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" width="34" height="34">
           <path d="M19.9 11.7l-2.7-3c-.2-.2-.5-.2-.7 0-.1.1-.1.2-.1.3v2H5c-.6 0-1 .4-1 1s.4 1 1 1h11v2c0 .2.1.4.3.5h.2c.1 0 .3-.1.4-.2l2.7-3c0-.2 0-.4-.1-.6z"></path>
           </svg>
-          ).html_safe
+          )
         end
       end
     end
