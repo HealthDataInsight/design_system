9124c12ff3b862565747328e92c1f17a4bb35551
shilpigoel <shilpi.goel1@nhs.net>
2024-12-13 09:58:09 +0000

    Updated brand derivable test

--- test/builders/concerns/brand_derivable_test.rb
+++ test/builders/concerns/brand_derivable_test.rb
@@ -2,21 +2,18 @@
 
 require 'test_helper'
 require Rails.root.join('../../lib/design_system/builders/concerns/brand_derivable')
-require Rails.root.join('../../lib/design_system/builders/govuk/button')
-require Rails.root.join('../../lib/design_system/builders/nhsuk/button')
 
 module DesignSystem
   module Builders
+    class HelloWorld
+      include DesignSystem::Builders::Concerns::BrandDerivable
+    end
+
     module Concerns
       class BrandDerivableTest < ActionView::TestCase
         test 'test brand name derived from class name' do
-          instance = DesignSystem::Builders::Govuk::Button.new(nil)
-
-          assert_equal 'govuk', instance.brand
-
-          instance = DesignSystem::Builders::Nhsuk::Button.new(nil)
-
-          assert_equal 'nhsuk', instance.brand
+          instance = DesignSystem::Builders::HelloWorld.new
+          assert_equal 'hello_world', instance.brand
         end
       end
     end
