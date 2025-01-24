8ea7cc41a87cc06c08c2137846e05bdf7c0ce145
FilisLiu <filis.liu@healthdatainsight.org.uk>
2025-01-24 11:08:19 +0000

    cleanup

--- test/helpers/hdi_helper_test.rb
+++ test/helpers/hdi_helper_test.rb
@@ -3,34 +3,6 @@ require 'test_helper'
 class HdiHelperTest < ActionView::TestCase
   include HdiHelper
 
-  # test 'assistant_image_tag' do
-  #   @output_buffer = ActionView::OutputBuffer.new(assistant_image_tag(assistant))
-  #   TODO: Add assertions
-
-  #   # Big
-  #   @output_buffer = ActionView::OutputBuffer.new(assistant_image_tag(assistant, true))
-  #   TODO: Add assertions
-  # end
-
-  # test 'current_user_image_tag' do
-  #   @output_buffer = ActionView::OutputBuffer.new(current_user_image_tag)
-  #   TODO: Add assertions
-
-  #   # Big
-  #   @output_buffer = ActionView::OutputBuffer.new(current_user_image_tag(true))
-  #   TODO: Add assertions
-  # end
-
-  # test 'render_code_block' do
-  #   @output_buffer = ActionView::OutputBuffer.new(render_code_block(code, language))
-  #   TODO: Add assertions
-  # end
-
-  # test 'render_markdown' do
-  #   @output_buffer = ActionView::OutputBuffer.new(render_markdown(markdown_text))
-  #   TODO: Add assertions
-  # end
-
   test 'sidebar_navigation_svg' do
     @output_buffer = ActionView::OutputBuffer.new(
       hdi_sidebar_navigation_svg('Home', root_path, true, data: { test: 'foo' }) do
@@ -54,13 +26,4 @@ class HdiHelperTest < ActionView::TestCase
       end
     end
   end
-
-  # test 'unknown_user_image_tag' do
-  #   @output_buffer = ActionView::OutputBuffer.new(unknown_user_image_tag)
-  #   TODO: Add assertions
-
-  #   # Big
-  #   @output_buffer = ActionView::OutputBuffer.new(unknown_user_image_tag(true))
-  #   TODO: Add assertions
-  # end
 end
