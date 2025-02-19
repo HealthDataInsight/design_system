5e74b047fda7e42d2ec7db7f40cdeb96d4f7bdb0
Tim Gentry <52189+timgentry@users.noreply.github.com>
2025-02-19 16:27:59 +0000

    Linting

--- app/assets/javascripts/design_system/controllers/index.js
+++ app/assets/javascripts/design_system/controllers/index.js
@@ -2,8 +2,6 @@
 import HelloWorldController from "./hello_world_controller"
 // import OtherController from "./other_controller"
 
-console.log('loaded')
-
 export const registerControllers = (application) => {
   application.register("ds--hello-world", HelloWorldController)
   // application.register("ds--other", OtherController)
