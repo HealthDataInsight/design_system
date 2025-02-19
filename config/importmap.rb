c68e7420743687dadb20f29c84970dcb44d23aaa
shilpigoel <shilpi.goel1@nhs.net>
2025-02-19 17:13:11 +0000

    Initial commit

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,1 +1,4 @@
 pin 'design_system/controllers', to: 'design_system/controllers/index.js'
+
+pin 'javascript-time-ago', to: 'https://ga.jspm.io/npm:javascript-time-ago@2.5.11/index.js'
+pin 'relative-time-format', to: 'https://ga.jspm.io/npm:relative-time-format@1.1.6/index.js'
