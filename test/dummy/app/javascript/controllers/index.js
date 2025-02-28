be27f1781564bd4064f565242b954a5dfc81e58f
shilpigoel <shilpi.goel1@nhs.net>
2025-02-28 10:29:46 +0000

    Using local stimulus-loading.js

--- test/dummy/app/javascript/controllers/index.js
+++ test/dummy/app/javascript/controllers/index.js
@@ -1,6 +1,6 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+import { eagerLoadControllersFrom } from "ds-stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 
 import { registerControllers } from "design_system/controllers"
