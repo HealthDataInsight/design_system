486a4675a3af7d751e05e7a28bc9407126466cb8
FilisLiu <filis.liu@healthdatainsight.org.uk>
2025-03-04 17:44:54 +0000

    Add margin bottom

--- lib/design_system/builders/hdi/elements/headings.rb
+++ lib/design_system/builders/hdi/elements/headings.rb
@@ -7,15 +7,15 @@ module DesignSystem
           private
 
           def render_main_heading
-            content_tag(:h1, @main_heading, class: 'text-3xl font-bold tracking-tight sm:text-4xl text-gray-900')
+            content_tag(:h1, @main_heading, class: 'text-3xl font-bold tracking-tight sm:text-4xl text-gray-900 mb-4')
           end
 
           def render_subheading
-            content_tag(:h2, @subheading, class: 'text-2xl font-semibold leading-6 text-gray-900')
+            content_tag(:h2, @subheading, class: 'text-2xl font-semibold leading-6 text-gray-900 mb-4')
           end
 
           def render_caption
-            content_tag(:span, @caption, class: 'mt-2 text-sm hdi-text-700')
+            content_tag(:span, @caption, class: 'text-sm hdi-text-500 mb-4')
           end
         end
       end
