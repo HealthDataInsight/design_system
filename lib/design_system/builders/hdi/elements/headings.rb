c25731b9e7fcc54fd3d402a4d16df1976852d2a7
FilisLiu <filis.liu@healthdatainsight.org.uk>
2025-03-06 17:46:45 +0000

    Responsive text for heading helpers

--- lib/design_system/builders/hdi/elements/headings.rb
+++ lib/design_system/builders/hdi/elements/headings.rb
@@ -7,7 +7,12 @@ module DesignSystem
           private
 
           def render_main_heading
-            content_tag(:h1, @main_heading, class: 'text-3xl font-bold tracking-tight sm:text-4xl text-gray-900 mb-4')
+            content_tag(:h1, @main_heading,
+                        class: 'text-3xl font-bold tracking-tight sm:text-4xl text-gray-900 mb-2 break-words')
+          end
+
+          def render_caption
+            content_tag(:span, @caption, class: 'text-xs sm:text-sm hdi-text-500 mt-2 mb-2 block')
           end
         end
       end
--- lib/design_system/builders/hdi/heading.rb
+++ lib/design_system/builders/hdi/heading.rb
@@ -5,11 +5,11 @@ module DesignSystem
     module Hdi
       # This class provides Hdi methods to display headings in page content.
       class Heading < ::DesignSystem::Builders::Generic::Heading
-        def render_heading(text, level: 2, caption: nil)
+        def render_heading(text, level: 2)
           validate_level(level)
 
-          content = caption ? "#{text} #{content_tag(:span, caption, class: 'text-sm hdi-text-500')}".html_safe : text
-          content_tag("h#{level}", content, class: 'text-2xl font-semibold leading-6 text-gray-900')
+          content_tag("h#{level}", text,
+                      class: 'text-lg sm:text-2xl font-semibold leading-6 text-gray-900 mt-2 mb-2 break-words')
         end
       end
     end
