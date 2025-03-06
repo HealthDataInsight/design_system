68de5fef2cb0d55fcbeb426da8455d797840f7e3
FilisLiu <filis.liu@healthdatainsight.org.uk>
2025-03-06 20:05:09 +0000

    Responsive table for nhsuk

--- lib/design_system/builders/nhsuk/table.rb
+++ lib/design_system/builders/nhsuk/table.rb
@@ -7,6 +7,21 @@ module DesignSystem
       class Table < ::DesignSystem::Builders::Govuk::Table
         private
 
+        def table_content
+          content_tag(:table, nil, class: "#{brand}-table-responsive") do
+            safe_buffer = ActiveSupport::SafeBuffer.new
+
+            if @table.caption
+              safe_buffer.concat(content_tag(:caption, @table.caption,
+                                             class: "#{brand}-table__caption"))
+            end
+            safe_buffer.concat(render_headers)
+            safe_buffer.concat(render_rows)
+
+            safe_buffer
+          end
+        end
+
         def render_headers
           content_tag(:thead, role: 'rowgroup', class: "#{brand}-table__head") do
             content_tag(:tr, role: 'row') do
@@ -27,8 +42,21 @@ module DesignSystem
         def render_row(row)
           content_tag(:tr, role: 'row', class: "#{brand}-table__row") do
             row.each_with_object(ActiveSupport::SafeBuffer.new).with_index do |(cell, cell_buffer), index|
-              cell_buffer.concat(render_cell(cell, index, 'row'))
+              cell_buffer.concat(render_data_cell(cell, index))
             end
+          end
+        end
+
+        def render_data_cell(cell, index)
+          classes = "#{brand}-table__cell"
+          classes += " #{brand}-table__cell--numeric" if cell_numeric?(cell)
+
+          content_tag(:td, cell[:options].merge(class: classes), role: 'cell') do
+            buffer = ActiveSupport::SafeBuffer.new
+            buffer.concat(content_tag(:span, @table.columns[index][:content], class: "#{brand}-table-responsive__heading",
+                                                                              aria: { hidden: true }))
+            buffer.concat(cell[:content].to_s)
+            buffer
           end
         end
       end
