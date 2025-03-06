f526a3668d364a4104324899853c6ed74159fbd0
FilisLiu <filis.liu@healthdatainsight.org.uk>
2025-03-06 20:46:58 +0000

    First version of hdi responsive table layout

--- lib/design_system/builders/hdi/table.rb
+++ lib/design_system/builders/hdi/table.rb
@@ -20,18 +20,29 @@ module DesignSystem
         private
 
         def table_content
-          content_tag(:table, class: 'min-w-full divide-y divide-gray-300') do
-            safe_buffer = ActiveSupport::SafeBuffer.new
-            safe_buffer.concat(content_tag(:caption, @table.caption, class: 'caption_top')) if @table.caption
-            safe_buffer.concat(render_headers)
-            safe_buffer.concat(render_rows)
+          safe_buffer = ActiveSupport::SafeBuffer.new
 
-            safe_buffer
-          end
+          # Big screen
+          safe_buffer.concat(content_tag(:table, class: 'min-w-full divide-y divide-gray-300') do
+            table_buffer = ActiveSupport::SafeBuffer.new
+            table_buffer.concat(content_tag(:caption, @table.caption, class: 'caption_top')) if @table.caption
+            table_buffer.concat(render_headers)
+            table_buffer.concat(render_rows)
+
+            table_buffer
+          end)
+
+          # Small screen
+          safe_buffer.concat(content_tag(:div, class: 'block sm:hidden w-full') do
+            render_rows_responsive
+          end)
+
+          safe_buffer
         end
 
+        # Big screen
         def render_headers
-          content_tag(:thead) do
+          content_tag(:thead, class: 'hidden sm:table-row-group') do
             content_tag(:tr) do
               @table.columns.each_with_object(ActiveSupport::SafeBuffer.new) do |cell, header_buffer|
                 header_buffer <<
@@ -44,7 +55,7 @@ module DesignSystem
         end
 
         def render_rows
-          content_tag(:tbody, class: 'divide-y divide-gray-200') do
+          content_tag(:tbody, class: 'divide-y divide-gray-200 hidden sm:table-row-group') do
             @table.rows.each_with_object(ActiveSupport::SafeBuffer.new) do |row, safe_buffer|
               safe_buffer.concat(render_row(row))
             end
@@ -59,6 +70,35 @@ module DesignSystem
                             cell[:options].merge(class: 'whitespace-nowrap px-3 py-4 text-sm text-gray-500'))
               )
             end
+          end
+        end
+
+        # Small screen
+        def render_rows_responsive
+          content_tag(:div, class: 'block w-full') do
+            @table.rows.each_with_object(ActiveSupport::SafeBuffer.new) do |row, buffer|
+              buffer.concat(render_row_responsive(row))
+            end
+          end
+        end
+
+        def render_row_responsive(row)
+          content_tag(:div, class: 'border-b border-gray-300 py-2') do
+            row.each_with_object(ActiveSupport::SafeBuffer.new).with_index do |(cell, cell_buffer), index|
+              cell_buffer.concat(render_data_cell(cell, index))
+            end
+          end
+        end
+
+        def render_data_cell(cell, index)
+          content_tag(:div, class: 'flex justify-between py-2 px-3') do
+            buffer = ActiveSupport::SafeBuffer.new
+            header_text = @table.columns[index][:content]
+
+            buffer.concat(content_tag(:span, header_text, class: 'font-semibold text-gray-700'))
+            buffer.concat(content_tag(:span, cell[:content], class: 'text-gray-500'))
+
+            buffer
           end
         end
       end
