1f5e7defc244ef24b0b0ce8138bbbb4ce4574de6
FilisLiu <filis.liu@healthdatainsight.org.uk>
2025-03-10 13:47:37 +0000

    Merge table render methods for hdi and cypress tests

--- lib/design_system/builders/hdi/table.rb
+++ lib/design_system/builders/hdi/table.rb
@@ -5,14 +5,13 @@ module DesignSystem
     module Hdi
       # This class is used to provide HDI Table.
       class Table < ::DesignSystem::Builders::Generic::Table
-        def render_table
+        def render_table(options = {})
           @table = ::DesignSystem::Components::Table.new
           yield @table
 
-          content_tag(:div) do
+          content_tag(:div, **options) do
             safe_buffer = ActiveSupport::SafeBuffer.new
             safe_buffer.concat(table_content)
-
             safe_buffer
           end
         end
@@ -22,7 +21,6 @@ module DesignSystem
         def table_content
           safe_buffer = ActiveSupport::SafeBuffer.new
 
-          # Big screen
           safe_buffer.concat(content_tag(:table, class: 'min-w-full divide-y divide-gray-300') do
             table_buffer = ActiveSupport::SafeBuffer.new
             table_buffer.concat(content_tag(:caption, @table.caption, class: 'caption_top')) if @table.caption
@@ -32,15 +30,9 @@ module DesignSystem
             table_buffer
           end)
 
-          # Small screen
-          safe_buffer.concat(content_tag(:div, class: 'block sm:hidden w-full') do
-            render_rows_responsive
-          end)
-
           safe_buffer
         end
 
-        # Big screen
         def render_headers
           content_tag(:thead, class: 'hidden sm:table-row-group') do
             content_tag(:tr) do
@@ -55,48 +47,28 @@ module DesignSystem
         end
 
         def render_rows
-          content_tag(:tbody, class: 'divide-y divide-gray-200 hidden sm:table-row-group') do
-            @table.rows.each_with_object(ActiveSupport::SafeBuffer.new) do |row, safe_buffer|
-              safe_buffer.concat(render_row(row))
+          content_tag(:tbody, class: 'divide-y divide-gray-200') do
+            @table.rows.each_with_object(ActiveSupport::SafeBuffer.new) do |row, buffer|
+              buffer.concat(render_row(row))
             end
           end
         end
 
         def render_row(row)
-          content_tag(:tr) do
-            row.each_with_object(ActiveSupport::SafeBuffer.new) do |cell, cell_buffer|
-              cell_buffer.concat(
-                content_tag(:td, cell[:content],
-                            cell[:options].merge(class: 'whitespace-nowrap px-3 py-4 text-sm text-gray-500'))
-              )
-            end
-          end
-        end
-
-        # Small screen
-        def render_rows_responsive
-          content_tag(:div, class: 'block w-full') do
-            @table.rows.each_with_object(ActiveSupport::SafeBuffer.new) do |row, buffer|
-              buffer.concat(render_row_responsive(row))
-            end
-          end
-        end
-
-        def render_row_responsive(row)
-          content_tag(:div, class: 'border-b border-gray-300 py-2') do
-            row.each_with_object(ActiveSupport::SafeBuffer.new).with_index do |(cell, cell_buffer), index|
-              cell_buffer.concat(render_data_cell(cell, index))
+          content_tag(:tr, class: 'whitespace-nowarp border-b border-gray-300 py-2') do
+            row.each_with_object(ActiveSupport::SafeBuffer.new).with_index do |(cell, buffer), index|
+              buffer.concat(render_data_cell(cell, index))
             end
           end
         end
 
         def render_data_cell(cell, index)
-          content_tag(:div, class: 'flex justify-between py-2 px-3') do
+          content_tag(:td, class: 'flex justify-between px-3 py-4 text-sm text-gray-500 sm:table-cell') do
             buffer = ActiveSupport::SafeBuffer.new
             header_text = @table.columns[index][:content]
 
-            buffer.concat(content_tag(:span, header_text, class: 'font-semibold text-gray-700'))
-            buffer.concat(content_tag(:span, cell[:content], class: 'text-gray-500'))
+            buffer.concat(content_tag(:span, header_text, class: 'font-semibold text-gray-700 sm:hidden'))
+            buffer.concat(cell[:content].to_s)
 
             buffer
           end
