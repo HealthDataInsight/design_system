e64103bbcd3472dc2914490b12c6fcd43536f19c
FilisLiu <filis.liu@healthdatainsight.org.uk>
2025-03-27 16:13:03 +0000

    Update seeds

--- test/dummy/db/seeds.rb
+++ test/dummy/db/seeds.rb
@@ -4,10 +4,11 @@ department2 = Department.find_or_create_by!(title: 'Marketing')
 department3 = Department.find_or_create_by!(title: 'Finance')
 
 # Adding a first group of assistants
-assistant1 = Assistant.find_or_create_by!(title: 'Lorem ipsum dolor sit amet', department: department1)
-assistant2 = Assistant.find_or_create_by!(title: 'consectetur adipisicing elit', department: department2)
-assistant3 = Assistant.find_or_create_by!(title: 'sed do eiusmod tempor incididunt', department: department3)
+assistant1 = Assistant.find_or_create_by!(title: 'AB', department: department1, role: 'User')
+assistant2 = Assistant.find_or_create_by!(title: 'CD', department: department2, role: 'Admin')
+assistant3 = Assistant.find_or_create_by!(title: 'EF', department: department3, role: 'Super Admin')
 
-assistant1.features.create!(name: 'Feature 1', description: 'Description 1')
-assistant1.features.create!(name: 'Feature 2', description: 'Description 2')
-assistant2.features.create!(name: 'Feature 3', description: 'Description 3')
+# Adding a first group of features
+feature1 = Feature.find_or_create_by!(name: 'Feature 1', description: 'Description 1', assistant: assistant1)
+feature2 = Feature.find_or_create_by!(name: 'Feature 2', description: 'Description 2', assistant: assistant2)
+feature3 = Feature.find_or_create_by!(name: 'Feature 3', description: 'Description 3', assistant: assistant3)
