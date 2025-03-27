ab624e20e9aea936614f6fd3518d5eaf3d32d310
FilisLiu <filis.liu@healthdatainsight.org.uk>
2025-03-27 16:14:05 +0000

    Add validations to new columns and fix association id issue

--- test/dummy/app/models/assistant.rb
+++ test/dummy/app/models/assistant.rb
@@ -1,7 +1,12 @@
 # This is a demonstration class for testing the design system.
 class Assistant < ApplicationRecord
-  belongs_to :department
+  # Rails now adds presence validation to associations automatically but usually govuk-form-builder set relationships by assigning values to the foreign key column.
+  # This results in errors being added to the object on attributes that do not appear in the form, for example on department instead of department_id.
+  # You can suppress this behaviour by adding optional: true to the relationship and manually adding the presence validation to the foreign key field yourself.
+  belongs_to :department, optional: true
   has_many :features
 
   validates :title, presence: true
+  validates :department_id, presence: true
+  validates :role, presence: true
 end
