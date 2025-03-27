d80bf7ad0e0cd3cb7b5db9b77d1004aec0b213e1
FilisLiu <filis.liu@healthdatainsight.org.uk>
2025-03-27 17:05:36 +0000

    Change is_active to terms_agreed

--- test/dummy/db/migrate/20240515214315_create_assistants.rb
+++ test/dummy/db/migrate/20240515214315_create_assistants.rb
@@ -1,7 +1,7 @@
 class CreateAssistants < ActiveRecord::Migration[7.1]
   def change
     create_table :assistants do |t|
-      t.boolean :is_active, default: false
+      t.boolean :terms_agreed, default: false
       t.date :date_of_birth
       t.references :department, null: false, foreign_key: true
       t.string :role, null: false
