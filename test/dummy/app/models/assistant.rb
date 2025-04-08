b3f6f28c69f9282c0951e306fe0e90014ad3b3ad
FilisLiu <filis.liu@healthdatainsight.org.uk>
2025-04-08 12:33:12 +0100

    Complete validations and tests

--- test/dummy/app/models/assistant.rb
+++ test/dummy/app/models/assistant.rb
@@ -13,18 +13,36 @@ class Assistant < ApplicationRecord
     Colour.new('blue', 'Blue', 'Violets are... purple?')
   ].freeze
 
+  serialize :desired_filling, JSON
+
   # Rails now adds presence validation to associations automatically but usually govuk-form-builder set relationships by assigning values to the foreign key column.
   # This results in errors being added to the object on attributes that do not appear in the form, for example on department instead of department_id.
   # You can suppress this behaviour by adding optional: true to the relationship and manually adding the presence validation to the foreign key field yourself.
   belongs_to :department, optional: true
   belongs_to :role, optional: true
 
-  validates :title,
-            presence: { message: 'Enter a title' },
-            length: { minimum: 2, message: 'Title should be longer than 1' }
+  validates :age,
+            presence: { message: 'Enter an age' },
+            numericality: { greater_than_or_equal_to: 0, message: 'Age must be greater than 0' }
+  validates :colour,
+            presence: { message: 'Select a colour' }
+  validates :description,
+            presence: { message: 'Enter a description' }
+  validates :desired_filling,
+            presence: { message: 'Select at least one filling' }
+  validates :lunch_option,
+            presence: { message: 'Select a lunch option' }
   validates :password,
             presence: { message: 'Enter a password' },
             length: { minimum: 8, message: 'Password must be longer than 8 characters' }
+  validates :terms_agreed,
+            acceptance: { message: 'You must agree to the terms and conditions' }
+  validates :title,
+            presence: { message: 'Enter a title' },
+            length: { minimum: 2, message: 'Title should be longer than 1' }
+  validates :website,
+            presence: { message: 'Enter a website' },
+            format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), message: 'Enter a valid website' }
 
   validates :department_id, presence: { message: 'Select a department' }
   validates :role_id, presence: { message: 'Select at least one role' }
--- test/models/assistant_test.rb
+++ test/models/assistant_test.rb
@@ -1,36 +1,80 @@
 require 'test_helper'
 
 class AssistantTest < ActiveSupport::TestCase
-  test 'validates title' do
+  test 'validates age' do
     assistant = Assistant.new
 
-    assistant.title = nil
+    assistant.age = nil
     refute assistant.valid?
-    assert_includes assistant.errors.details[:title], error: :blank
+    assert_includes assistant.errors.details[:age], error: :blank
 
-    assistant.title = ''
+    assistant.age = -1
     refute assistant.valid?
-    assert_includes assistant.errors.details[:title], error: :blank
+    assert_includes assistant.errors.details[:age], value: -1, error: :greater_than_or_equal_to, count: 0
 
-    assistant.title = 'a'
-    refute assistant.valid?
-    assert_includes assistant.errors.details[:title], error: :too_short, count: 2
-
-    assistant.title = 'foo'
+    assistant.age = 1
     assistant.valid?
-    assert_empty assistant.errors.details[:title]
+    assert_empty assistant.errors.details[:age]
   end
 
-  test 'validates department' do
+  test 'validates colour' do
     assistant = Assistant.new
 
-    assistant.department = nil
+    assistant.colour = nil
     refute assistant.valid?
-    assert_includes assistant.errors.details[:department_id], error: :blank
+    assert_includes assistant.errors.details[:colour], error: :blank
 
-    assistant.department = departments(:marketing)
+    assistant.colour = 'red'
     assistant.valid?
-    assert_empty assistant.errors.details[:department_id]
+    assert_empty assistant.errors.details[:colour]
+  end
+
+  test 'validates description' do
+    assistant = Assistant.new
+
+    assistant.description = nil
+    refute assistant.valid?
+    assert_includes assistant.errors.details[:description], error: :blank
+
+    assistant.description = 'a'
+    assistant.valid?
+    assert_empty assistant.errors.details[:description]
+  end
+
+  test 'validates desired filling' do
+    assistant = Assistant.new
+
+    assistant.desired_filling = nil
+    refute assistant.valid?
+    assert_includes assistant.errors.details[:desired_filling], error: :blank
+
+    assistant.desired_filling = []
+    refute assistant.valid?
+    assert_includes assistant.errors.details[:desired_filling], error: :blank
+
+    assistant.desired_filling = ['pastrami']
+    assistant.valid?
+    assert_empty assistant.errors.details[:desired_filling]
+    assert_equal ['pastrami'], assistant.desired_filling
+    assert_equal ['pastrami'], assistant.read_attribute(:desired_filling)
+
+    assistant.desired_filling = %w[pastrami cheddar]
+    assistant.valid?
+    assert_empty assistant.errors.details[:desired_filling]
+    assert_equal %w[pastrami cheddar], assistant.desired_filling
+    assert_equal '["pastrami","cheddar"]', assistant.read_attribute(:desired_filling)
+  end
+
+  test 'validates lunch option' do
+    assistant = Assistant.new
+
+    assistant.lunch_option = nil
+    refute assistant.valid?
+    assert_includes assistant.errors.details[:lunch_option], error: :blank
+
+    assistant.lunch_option = 'Salad'
+    assistant.valid?
+    assert_empty assistant.errors.details[:lunch_option]
   end
 
   test 'validates password' do
@@ -51,6 +95,74 @@ class AssistantTest < ActiveSupport::TestCase
     assistant.password = 'password'
     assistant.valid?
     assert_empty assistant.errors.details[:password]
+  end
+
+  test 'validates terms agreed' do
+    assistant = Assistant.new
+
+    assistant.terms_agreed = false
+    refute assistant.valid?
+    assert_includes assistant.errors.details[:terms_agreed], error: :accepted
+
+    assistant.terms_agreed = '0'
+    refute assistant.valid?
+    assert_includes assistant.errors.details[:terms_agreed], error: :accepted
+
+    assistant.terms_agreed = true
+    assistant.valid?
+    assert_empty assistant.errors.details[:terms_agreed]
+
+    assistant.terms_agreed = '1'
+    assistant.valid?
+    assert_empty assistant.errors.details[:terms_agreed]
+  end
+
+  test 'validates title' do
+    assistant = Assistant.new
+
+    assistant.title = nil
+    refute assistant.valid?
+    assert_includes assistant.errors.details[:title], error: :blank
+
+    assistant.title = ''
+    refute assistant.valid?
+    assert_includes assistant.errors.details[:title], error: :blank
+
+    assistant.title = 'a'
+    refute assistant.valid?
+    assert_includes assistant.errors.details[:title], error: :too_short, count: 2
+
+    assistant.title = 'foo'
+    assistant.valid?
+    assert_empty assistant.errors.details[:title]
+  end
+
+  test 'validates website' do
+    assistant = Assistant.new
+
+    assistant.website = nil
+    refute assistant.valid?
+    assert_includes assistant.errors.details[:website], error: :blank
+
+    assistant.website = 'not a website'
+    refute assistant.valid?
+    assert_includes assistant.errors.details[:website], error: :invalid, value: 'not a website'
+
+    assistant.website = 'http://www.example.com'
+    assistant.valid?
+    assert_empty assistant.errors.details[:website]
+  end
+
+  test 'validates department' do
+    assistant = Assistant.new
+
+    assistant.department = nil
+    refute assistant.valid?
+    assert_includes assistant.errors.details[:department_id], error: :blank
+
+    assistant.department = departments(:marketing)
+    assistant.valid?
+    assert_empty assistant.errors.details[:department_id]
   end
 
   test 'validates role' do
