114070ef582d9c0594148a454f3c7ceb66d7847b
FilisLiu <filis.liu@healthdatainsight.org.uk>
2025-04-10 11:43:25 +0100

    Updated schema

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,30 +11,36 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2025_04_01_134939) do
-  create_table "assistants", force: :cascade do |t|
-    t.boolean "terms_agreed", default: false
-    t.date "date_of_birth"
-    t.integer "age"
-    t.string "colour"
-    t.string "desired_filling"
-    t.string "email"
-    t.string "lunch_option"
-    t.string "password"
-    t.string "phone"
-    t.string "title"
-    t.string "website"
-    t.text "description"
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "department_id"
-    t.integer "role_id"
-    t.index ["department_id"], name: "index_assistants_on_department_id"
-    t.index ["role_id"], name: "index_assistants_on_role_id"
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "departments", force: :cascade do |t|
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "assistants", force: :cascade do |t|
     t.string "title"
-    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "department_id", null: false
@@ -67,6 +73,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_01_134939) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "assistants", "departments"
   add_foreign_key "assistants", "roles"
 end
