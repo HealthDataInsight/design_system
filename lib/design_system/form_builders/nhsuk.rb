812f2664d1597ef1884d3cedce444cf9727ee0a1
FilisLiu <138935059+FilisLiu@users.noreply.github.com>
2025-04-14 22:13:37 +0100

    Feature/nhs password error (#83)
    * Copy files from feature/form-with-frontend
    
    * Revert hdi.rb
    
    * Test for nhsuk password field with error
    
    * Fix

--- lib/design_system/form_builders/nhsuk.rb
+++ lib/design_system/form_builders/nhsuk.rb
@@ -7,7 +7,10 @@ module DesignSystem
     class Nhsuk < Govuk
       def ds_password_field(method, options = {})
         @brand = config.brand
-        password_field_options = css_class_options_merge(options, ["#{@brand}-input"])
+        options[:id] = govuk_field_id(method, link_errors: true)
+        password_field_options = css_class_options_merge(options,
+                                                         ["#{@brand}-input",
+                                                          has_errors?(method) ? "#{@brand}-input--error" : nil].compact)
 
         hint = options.delete(:hint)
         password_field_options[:'aria-describedby'] = field_id("#{method}-hint") if hint
@@ -15,7 +18,10 @@ module DesignSystem
         password_field_options['data-ds--show-password-target'] = 'password'
         password_field_options.delete('text')
 
-        content_tag(:div, class: "#{@brand}-form-group", 'data-controller': 'ds--show-password') do
+        form_group_classes = ["#{@brand}-form-group"]
+        form_group_classes << "#{@brand}-form-group--error" if has_errors?(method)
+
+        content_tag(:div, class: form_group_classes.join(' '), 'data-controller': 'ds--show-password') do
           ds_label(method, {}) +
             optional_hint(method, hint) +
             password_field(method, password_field_options) + '&nbsp;'.html_safe +
