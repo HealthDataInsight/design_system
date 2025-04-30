d9f02e768f947d3da17a63944a68c6366dc18217
FilisLiu <filis.liu@healthdatainsight.org.uk>
2025-04-30 17:02:20 +0100

    move svg inside password controller

--- lib/design_system/form_builders/hdi.rb
+++ lib/design_system/form_builders/hdi.rb
@@ -8,18 +8,13 @@ module DesignSystem
       # In this instance the string is safe, because it contains known text.
       # rubocop:disable Rails/OutputSafety
       SHOW_PASSWORD_SVG = <<~SVG.html_safe
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="hdi-password-field__icon">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="password-field__icon">
           <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
           <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
         </svg>
       SVG
-
-      HIDE_PASSWORD_SVG = <<~SVG.html_safe
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="hdi-password-field__icon">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
-        </svg>
-      SVG
       # rubocop:enable Rails/OutputSafety
+
       def ds_password_field(method, options = {})
         @brand = config.brand
         options[:id] = govuk_field_id(method, link_errors: true)
@@ -36,12 +31,7 @@ module DesignSystem
         form_group_classes = ["#{@brand}-form-group"]
         form_group_classes << "#{@brand}-form-group--error" if has_errors?(method)
 
-        content_tag(:div, class: form_group_classes.join(' '),
-                          data: {
-                            controller: 'ds--show-password',
-                            'ds--show-password-show-content-value': SHOW_PASSWORD_SVG,
-                            'ds--show-password-hide-content-value': HIDE_PASSWORD_SVG
-                          }) do
+        content_tag(:div, class: form_group_classes.join(' '), 'data-controller': 'ds--show-password') do
           ds_label(method, {}) +
             optional_hint(method, hint) +
             content_tag(:div, class: "#{@brand}-password-field") do
