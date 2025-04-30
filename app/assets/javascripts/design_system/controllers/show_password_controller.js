2629198b3a3abfefa4e3cd6bf064a6e1ba16d318
FilisLiu <filis.liu@healthdatainsight.org.uk>
2025-04-30 16:10:10 +0100

    password controller with configurable show/hide text and supports svg

--- app/assets/javascripts/design_system/controllers/show_password_controller.js
+++ app/assets/javascripts/design_system/controllers/show_password_controller.js
@@ -2,14 +2,29 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = [ 'password' , 'button']
+  static values = {
+    showContent: String,
+    hideContent: String,
+  }
 
   toggle () {
     if (this.passwordTarget.type === 'password') {
       this.passwordTarget.type = 'text'
-	    this.buttonTarget.textContent = 'Hide password'
+      this.updateButtonContent(this.hideContentValue)
     } else {
       this.passwordTarget.type = 'password'
-	    this.buttonTarget.textContent = 'Show password'
+      this.updateButtonContent(this.showContentValue)
+    }
+  }
+
+  updateButtonContent(content) {
+    // Check if content contains HTML tags
+    const hasHtml = /<[a-z][\s\S]*>/i.test(content)
+    
+    if (hasHtml) {
+      this.buttonTarget.innerHTML = content
+    } else {
+      this.buttonTarget.textContent = content
     }
   }
 }
