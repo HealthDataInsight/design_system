6747064e03360811997f9a52f58444a17a75fdda
FilisLiu <filis.liu@healthdatainsight.org.uk>
2025-05-01 13:22:07 +0100

    hdi caption

--- lib/design_system/builders/hdi/elements/headings.rb
+++ lib/design_system/builders/hdi/elements/headings.rb
@@ -6,20 +6,8 @@ module DesignSystem
         module Headings
           private
 
-          def render_main_heading
-            safe_buffer = ActiveSupport::SafeBuffer.new
-
-            # HDI renders caption below the main heading.
-            # Longer captions up to one or two sentences are accepted.
-            safe_buffer.concat(DesignSystem::Registry.builder(brand, 'heading', self).render_heading(@main_heading,
-                                                                                                     level: 1))
-            safe_buffer.concat(render_caption) if @caption
-
-            safe_buffer
-          end
-
           def render_caption
-            content_tag(:span, @caption, class: 'text-xs sm:text-sm hdi-text-500 mt-2 mb-2 block')
+            content_tag(:span, @caption, class: "#{brand}-caption-m")
           end
         end
       end
