74c155d0afd669e213ca95503f71b715e554d21a
Tim Gentry <52189+timgentry@users.noreply.github.com>
2025-07-23 08:06:40 +0100

    Bump to HDI Frontend 0.12.0

--- lib/design_system/engine.rb
+++ lib/design_system/engine.rb
@@ -23,7 +23,7 @@ module DesignSystem
         urls: [
           '/design_system/static/date-fns-4.1.0',
           '/design_system/static/govuk-frontend-5.9.0',
-          '/design_system/static/hdi-frontend-0.11.0',
+          '/design_system/static/hdi-frontend-0.12.0',
           '/design_system/static/heroicons-2.1.5',
           '/design_system/static/ndrsuk-frontend-9.3.0',
           '/design_system/static/nhsuk-frontend-9.3.0',
