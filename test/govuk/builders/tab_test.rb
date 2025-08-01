57fe817c610b62c5b642337d8a4082428034667d
shilpigoel <shilpi.goel1@nhs.net>
2025-08-01 17:59:51 +0100

    Tab component able to capture block content

--- test/govuk/builders/tab_test.rb
+++ test/govuk/builders/tab_test.rb
@@ -16,13 +16,26 @@ module DesignSystem
 
         test 'rendering govuk tab' do
           @output_buffer = ds_tab do |tab|
-            tab.add_tab_panel('Test', 'test paragraph', 'test', sel: true)
+            tab.add_tab_panel('Test', 'test paragraph', 'test', selected: true)
             tab.add_tab_panel('Trial', 'trial paragraph', 'trial')
           end
 
           assert_select('ul.govuk-tabs__list li', 2)
           assert_select('a.govuk-tabs__tab', text: 'Test')
           assert_select('div.govuk-tabs__panel', text: 'test paragraph')
+        end
+
+        test 'rendering govuk tab with html' do
+          @output_buffer = ds_tab do |tab|
+            tab.add_tab_panel('Test', nil, 'test', selected: true) do
+              tag.p('a paragraph')
+            end
+            tab.add_tab_panel('Trial', 'trial paragraph', 'trial')
+          end
+
+          assert_select('ul.govuk-tabs__list li', 2)
+          assert_select('a.govuk-tabs__tab', text: 'Test')
+          assert_select('div.govuk-tabs__panel p', text: 'a paragraph')
         end
       end
     end
