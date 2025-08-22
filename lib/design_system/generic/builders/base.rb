746d86bb5c7212176daff08584b9837db638c481
Tim Gentry <52189+timgentry@users.noreply.github.com>
2025-08-22 15:11:19 +0100

    Linting

--- lib/design_system/generic/builders/base.rb
+++ lib/design_system/generic/builders/base.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
+require 'design_system/helpers/css_helper'
+
 module DesignSystem
   module Generic
     module Builders
       # This is the base class for design system builders.
       class Base
-        include DesignSystem::Helpers::CssHelper
         include DesignSystem::Generic::Builders::Concerns::BrandDerivable
+        include DesignSystem::Helpers::CssHelper
+
         delegate :button_tag, :capture, :content_for, :content_tag, :link_to, :link_to_unless_current, to: :@context
 
         def initialize(context)
