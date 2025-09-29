f9d5c216bc0b9b41f268be7ee8a21d63528a3dee
shilpigoel <shilpi.goel1@nhs.net>
2025-09-29 11:28:53 +0100

    Configurable searchbar

--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 # This is the application controller
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session if Rails.env.test?
 
   include DesignSystem::Branded
 
-  before_action :add_navigation, :set_service_name, :set_footer_links
+  before_action :add_navigation, :set_service_name, :set_footer_links, :set_searchbar_presence
   helper_method :brand
 
   private
@@ -30,5 +32,9 @@ class ApplicationController < ActionController::Base
     add_footer_link('Custom Link', '#', target: '_blank', rel: 'noopener')
     add_footer_link('Another Link', '#')
     self.copyright_notice = '© NHS England 2025'
+  end
+
+  def set_searchbar_presence
+    @searchbar = false # Default is hidden
   end
 end
