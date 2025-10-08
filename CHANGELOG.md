5aba45c9f316b9e1e8e302cda796dd0cd00df21b
shilpigoeldev <83065526+shilpigoeldev@users.noreply.github.com>
2025-10-08 15:06:41 +0100

    making Zeitwerk happy (#194)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## Fixed
+
+- Adding a stub class for all.rb to make zeitwerk happy
+
 ## [0.9.1] - 2025-10-08
 
 ### Fixed
--- lib/design_system/all.rb
+++ lib/design_system/all.rb
@@ -1,3 +1,10 @@
 require_relative 'generic'
 require_relative 'govuk'
 require_relative 'nhsuk'
+module DesignSystem
+  # Make zeitwerk happy by defining a stub class
+  # rubocop:disable Lint/EmptyClass
+  class All
+  end
+  # rubocop:enable Lint/EmptyClass
+end
