817c43ffec23fc1ead1dbfe43b3efa4df479a60f
Tim Gentry <52189+timgentry@users.noreply.github.com>
2025-12-15 14:26:46 +0000

    Check that the consuming app is using importmap

--- lib/design_system/engine.rb
+++ lib/design_system/engine.rb
@@ -5,7 +5,7 @@ module DesignSystem
   # This is the main engine class for the design system.
   class Engine < ::Rails::Engine
     initializer 'design_system.importmap', before: 'importmap' do |app|
-      app.config.importmap.paths << Engine.root.join('config/importmap.rb')
+      app.config.importmap.paths << Engine.root.join('config/importmap.rb') if app.config.respond_to?(:importmap)
     end
 
     # Adding Rack::Static to serve up assets from the design_systems
