1d8e783c6b0bfe7fde9945f6adc74f054fc610fa
FilisLiu <filis.liu@healthdatainsight.org.uk>
2026-01-23 17:57:02 +0000

    backlink as fixed element

---
+++ lib/design_system/generic/builders/elements/backlink.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module DesignSystem
+  module Generic
+    module Builders
+      module Elements
+        # This mixin module is used to provide back link.
+        module Backlink
+          def backlink(label, path)
+            @backlink = true
+            @label = label
+            @path = path
+          end
+
+          private
+
+          def content_for_backlink
+            content_for(:backlink) do
+              render_backlink
+            end
+          end
+
+          def render_backlink(label = @label, path = @path)
+            content_tag(:a, label, href: path, class: "#{brand}-back-link")
+          end
+        end
+      end
+    end
+  end
+end
