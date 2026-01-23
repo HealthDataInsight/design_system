067ec039854bb8f7b860c2e7f93b3647da8991d4
FilisLiu <filis.liu@healthdatainsight.org.uk>
2026-01-23 15:09:20 +0000

    Tests

---
+++ test/govuk/builders/back_link_test.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module DesignSystem
+  module Govuk
+    module Builders
+      # This tests the govuk back link builder
+      class BackLinkTest < ActionView::TestCase
+        include DesignSystemHelper
+
+        setup do
+          @brand = 'govuk'
+          @controller.stubs(:brand).returns(@brand)
+          @assistant = assistants(:one)
+        end
+
+        test 'rendering govuk link' do
+          @output_buffer = ds_back_link(assistant_path(@assistant))
+
+          assert_select("a.#{@brand}-back-link", href: assistant_path(@assistant))
+        end
+
+        test 'rendering govuk button link and custom text' do
+          @output_buffer = ds_back_link(assistants_path, 'Custom text')
+
+          assert_select("a.#{@brand}-back-link", href: assistants_path, text: 'Custom text')
+        end
+      end
+    end
+  end
+end
