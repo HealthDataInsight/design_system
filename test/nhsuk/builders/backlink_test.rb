b6fef9f172d45c698e2354fb9d4b875bf789cb0b
FilisLiu <filis.liu@healthdatainsight.org.uk>
2026-02-02 11:17:57 +0000

    update test

--- test/nhsuk/builders/backlink_test.rb
+++ test/nhsuk/builders/backlink_test.rb
@@ -21,14 +21,35 @@ module DesignSystem
           end
 
           @output_buffer = @view_flow.get(:backlink)
-          assert_select('nav') do
-            assert_select("div.#{@brand}-width-container") do
-              assert_select("div.#{@brand}-back-link") do
-                assert_select("a.#{@brand}-back-link__link", href: assistant_path(@assistant), text: 'Back')
-                assert_select("svg.#{@brand}-icon.#{@brand}-icon__chevron-left")
+          assert_select("a.#{@brand}-back-link", href: assistant_path(@assistant), text: 'Back')
+        end
+
+        test 'rendering nhsuk backlink only, with label' do
+          ds_fixed_elements do |ds|
+            ds.backlink 'Custom text', assistant_path(@assistant)
+          end
+
+          @output_buffer = @view_flow.get(:backlink)
+          assert_select("a.#{@brand}-back-link", href: assistant_path(@assistant), text: 'Custom text')
+        end
+
+        test 'rendering nhsuk backlink with breadcrumbs' do
+          ds_fixed_elements do |ds|
+            ds.breadcrumb 'One', root_path
+            ds.backlink 'Custom text', assistant_path(@assistant)
+          end
+
+          # backlink should not coexist with breadcrumbs
+          @output_buffer = @view_flow.get(:breadcrumbs)
+          assert_select("nav.#{@brand}-breadcrumb") do
+            assert_select("ol.#{@brand}-breadcrumb__list", role: 'listz') do
+              assert_select("li.#{@brand}-breadcrumb__list-item") do
+                assert_select("a.#{@brand}-breadcrumb__link", href: root_url, text: 'One')
               end
             end
           end
+          @output_buffer = @view_flow.get(:backlink)
+          assert_select("a.#{@brand}-back-link", false)
         end
       end
     end
