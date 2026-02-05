58d511cb0f1d252cb51ee7c92ae4d5009d08d50e
FilisLiu <138935059+FilisLiu@users.noreply.github.com>
2026-02-05 09:35:30 +0000

    ds_notice with success banner and custom banner text (#240)
    * notification type hash
    
    * Refactor notification type handling
    
    * tests
    
    * demo
    
    * raise exception if type not valid
    
    * Streamlining and raise exception

--- lib/design_system/generic/builders/notification.rb
+++ lib/design_system/generic/builders/notification.rb
@@ -16,23 +16,28 @@ module DesignSystem
           end
         end
 
-        def render_notice(msg = nil, &)
+        def render_notice(msg = nil, header: nil, type: :information, &)
           @context.instance_variable_set(:@link_context, :notification_banner)
 
+          raise ArgumentError,
+                "Invalid notification type: #{type}. Must be one of: #{notification_type_hash.keys.join(', ')}" unless notification_type_hash.key?(type)
+
+          header ||= notification_type_hash.dig(type, :header)
+
           content_to_display = block_given? ? capture(&) : msg
-          content_tag(:div, class: "#{brand}-notification-banner", role: 'region',
+          content_tag(:div, class: notification_type_hash.dig(type, :class), role: notification_type_hash.dig(type, :role),
                             'aria-labelledby': "#{brand}-notification-banner-title",
                             'data-module': "#{brand}-notification-banner") do
-            banner_tile + banner_content(content_to_display)
+            banner_tile(header) + banner_content(content_to_display)
           end
         end
 
         private
 
-        def banner_tile
+        def banner_tile(header)
           content_tag(:div, class: "#{brand}-notification-banner__header") do
-            content_tag(:h2, 'Important', class: "#{brand}-notification-banner__title",
-                                          id: "#{brand}-notification-banner-title")
+            content_tag(:h2, header, class: "#{brand}-notification-banner__title",
+                                     id: "#{brand}-notification-banner-title")
           end
         end
 
@@ -41,6 +46,21 @@ module DesignSystem
             content_tag(:p, content,
                         class: "#{brand}-notification-banner__heading")
           end
+        end
+
+        def notification_type_hash
+          {
+            information: {
+              header: 'Important',
+              class: "#{brand}-notification-banner",
+              role: 'region'
+            },
+            success: {
+              header: 'Success',
+              class: "#{brand}-notification-banner #{brand}-notification-banner--success",
+              role: 'alert'
+            }
+          }
         end
       end
     end
