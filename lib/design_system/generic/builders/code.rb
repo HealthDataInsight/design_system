ccfa9c89a6f6855174e1bbf4a6b10b883872308f
FilisLiu <filis.liu@healthdatainsight.org.uk>
2026-02-24 17:25:46 +0000

    mirror nhsuk preview structure

--- lib/design_system/generic/builders/code.rb
+++ lib/design_system/generic/builders/code.rb
@@ -3,13 +3,15 @@
 module DesignSystem
   module Generic
     module Builders
-      # This class provides generic methods to display code blocks.
+      # Renders a code block with copy button.
       class Code < Base
         def render_code(code, language)
-          content_tag(:div, class: 'app-example-code', data: { controller: 'ds--clipboard' }) do
-            content_tag(:button, 'Copy', class: 'app-copy-button', data: { action: 'click->ds--clipboard#copy', target: 'ds--clipboard.buttonText' }) +
-            content_tag(:pre, data: { target: 'ds--clipboard.source' }) do
-              content_tag(:code, code, data: { controller: 'ds--code-highlight' }, class: "hljs language-#{language}")
+          content_tag(:div, class: 'app-example__code', data: { controller: 'ds--clipboard' }) do
+            content_tag(:button, 'Copy', class: 'app-example__copy-button', data: { action: 'click->ds--clipboard#copy', target: 'ds--clipboard.buttonText' }) +
+            content_tag(:div, class: 'app-example__scroll') do
+              content_tag(:pre, data: { target: 'ds--clipboard.source' }) do
+                content_tag(:code, code, data: { controller: 'ds--code-highlight' }, class: "hljs language-#{language}")
+              end
             end
           end
         end
