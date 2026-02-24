a782dad156e07ddc0062d30f181e3f7a54f63ff0
FilisLiu <filis.liu@healthdatainsight.org.uk>
2026-02-24 17:39:39 +0000

    linting

--- lib/design_system/generic/builders/code.rb
+++ lib/design_system/generic/builders/code.rb
@@ -7,12 +7,14 @@ module DesignSystem
       class Code < Base
         def render_code(code, language)
           content_tag(:div, class: 'app-example__code', data: { controller: 'ds--clipboard' }) do
-            content_tag(:button, 'Copy', class: 'app-example__copy-button', data: { action: 'click->ds--clipboard#copy', target: 'ds--clipboard.buttonText' }) +
-            content_tag(:div, class: 'app-example__scroll') do
-              content_tag(:pre, data: { target: 'ds--clipboard.source' }) do
-                content_tag(:code, code, data: { controller: 'ds--code-highlight' }, class: "hljs language-#{language}")
+            content_tag(:button, 'Copy', class: 'app-example__copy-button',
+                                         data: { action: 'click->ds--clipboard#copy', target: 'ds--clipboard.buttonText' }) +
+              content_tag(:div, class: 'app-example__scroll') do
+                content_tag(:pre, data: { target: 'ds--clipboard.source' }) do
+                  content_tag(:code, code, data: { controller: 'ds--code-highlight' },
+                                           class: "hljs language-#{language}")
+                end
               end
-            end
           end
         end
       end
