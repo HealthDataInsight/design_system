0842d0e55d53e56b2bb28febf381f6d6c7555b1f
FilisLiu <filis.liu@healthdatainsight.org.uk>
2026-03-03 14:22:03 +0000

    Hide demo attr; fixed multi-line ERB render (omit spaces); Extract fieldset

--- test/dummy/app/helpers/application_helper.rb
+++ test/dummy/app/helpers/application_helper.rb
@@ -5,17 +5,26 @@ require 'nokogiri'
 module ApplicationHelper
   def component_preview(html: nil, component: nil, id: nil, &block)
     erb_source = capture(&block)
+    display_source = hide_demo_attributes(erb_source)
+
     html ||= render(inline: erb_source)
-    html = render_form_component(html) if component == :form
+    html = extract_component(html, component) if component
     pretty_html = pretty_print(html)
 
     safe_buffer = ActiveSupport::SafeBuffer.new
-    safe_buffer << render_input(erb_source, id)
+    safe_buffer << render_input(display_source, id)
     safe_buffer << render_output(html, pretty_html, id)
     safe_buffer
   end
 
   private
+
+  def hide_demo_attributes(erb_source)
+    source = erb_source.to_s
+
+    # Strip hacks like html: { onsubmit: 'return false;' } that are used to prevent form submission.
+    source.gsub(/,\s*html:\s*\{\s*onsubmit:\s*'return false;'\s*\}/, '')
+  end
 
   def render_input(erb_source, id)
     ds_heading('Input', level: 4) +
@@ -34,14 +43,27 @@ module ApplicationHelper
 
   def pretty_print(html)
     fragment = Nokogiri::HTML.fragment(html)
+
+    fragment.traverse do |node|
+      node.remove if node.text? && node.text.strip.empty?
+    end
+
     fragment.children.map { |child| child.to_xml(indent: 2) }.join("\n")
   end
 
-  def render_form_component(html)
+  def extract_component(html, component)
     doc = Nokogiri::HTML.fragment(html)
-    form_group = doc.at_css("div.#{brand}-form-group")
-    return html unless form_group
 
-    form_group.to_xml(indent: 2).html_safe
+    target_tag = case component
+                 when :form_group
+                   "div.#{brand}-form-group"
+                 when :fieldset
+                   "fieldset.#{brand}-fieldset"
+                 end
+
+    extracted = doc.at_css(target_tag)
+    return html unless extracted
+
+    extracted.to_xml(indent: 2).html_safe
   end
 end
