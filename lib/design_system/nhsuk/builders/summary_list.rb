53abd87632cf6a4097142dfdfbe52688d150bf9f
FilisLiu <filis.liu@healthdatainsight.org.uk>
2026-03-09 15:43:03 +0000

    streamline nhsuk builder

--- lib/design_system/nhsuk/builders/summary_list.rb
+++ lib/design_system/nhsuk/builders/summary_list.rb
@@ -6,21 +6,6 @@ module DesignSystem
       class SummaryList < ::DesignSystem::Generic::Builders::SummaryList
         private
 
-        def validate_mix_actions
-          if @summary_list.rows.any? { |row| row[:actions].empty? } &&
-             @summary_list.rows.any? { |row| row[:actions].any? }
-            raise ArgumentError, "A mix of rows with and without actions is not supported for #{brand} style."
-          end
-        end
-
-        def render_rows
-          validate_mix_actions
-
-          content_tag(:dl, class: "#{brand}-summary-list") do
-            @summary_list.rows.map { |row| render_row(row) }.join.html_safe
-          end
-        end
-
         def render_hidden_text(hidden_text)
           return '' if hidden_text.blank?
 
