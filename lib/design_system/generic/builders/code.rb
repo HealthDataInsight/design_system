c9698c89d40585e05a53bd26cbd3d81daa4dde19
FilisLiu <filis.liu@healthdatainsight.org.uk>
2026-04-29 09:28:32 +0100

    update stimulus syntax

--- lib/design_system/generic/builders/code.rb
+++ lib/design_system/generic/builders/code.rb
@@ -8,9 +8,9 @@ module DesignSystem
         def render_code(code, language)
           content_tag(:div, class: 'app-example__code', data: { controller: 'ds--clipboard' }) do
             content_tag(:button, 'Copy', class: 'app-example__copy-button',
-                                         data: { action: 'click->ds--clipboard#copy', target: 'ds--clipboard.buttonText' }) +
+                                         data: { action: 'click->ds--clipboard#copy', 'ds--clipboard-target': 'buttonText' }) +
               content_tag(:div, class: 'app-example__scroll') do
-                content_tag(:pre, data: { target: 'ds--clipboard.source' }) do
+                content_tag(:pre, data: { 'ds--clipboard-target': 'source' }) do
                   content_tag(:code, code, data: { controller: 'ds--code-highlight' },
                                            class: "hljs language-#{language}")
                 end
