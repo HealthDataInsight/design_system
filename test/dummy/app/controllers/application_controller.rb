62c357312ba45565690d76947e24aae1ec914a23
FilisLiu <filis.liu@healthdatainsight.org.uk>
2026-07-13 12:47:46 +0100

    linting

--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -16,9 +16,7 @@ class ApplicationController < ActionController::Base
   end
 
   def add_navigation
-    if assistant_demo_enabled?
-      add_navigation_item('Manage Assistants', assistants_path, icon: 'users')
-    end
+    add_navigation_item('Manage Assistants', assistants_path, icon: 'users') if assistant_demo_enabled?
 
     add_navigation_item('GOV.UK', url_for(brand: 'govuk'), icon: 'ellipsis-horizontal-circle')
     add_navigation_item('NHS', url_for(brand: 'nhsuk'), icon: 'ellipsis-horizontal-circle')
