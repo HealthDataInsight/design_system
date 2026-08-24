3e78c1320f04ff4510cf99c42ce25bccd2839f59
FilisLiu <filis.liu@healthdatainsight.org.uk>
2026-08-24 12:56:40 +0100

    Update NHS button component styles and tests

--- app/components/design_system/nhsuk/button_component.rb
+++ app/components/design_system/nhsuk/button_component.rb
@@ -1,6 +1,14 @@
 module DesignSystem
   module Nhsuk
     class ButtonComponent < DesignSystem::Generic::ButtonComponent
+
+      def style_class_hash
+        {
+          'secondary' => "#{brand}-button--secondary",
+          'warning' => "#{brand}-button--warning",
+          'reverse' => "#{brand}-button--reverse"
+        }
+      end
     end
   end
 end
